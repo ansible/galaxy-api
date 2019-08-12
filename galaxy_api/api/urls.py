8af83904e92ff2d0bd788b24cd2bf4947dc29caf
Alexander Saprykin <osapryki@redhat.com>
2019-08-12 18:44:06 +0200

    Basic implementation of Pulp API in Galaxy
    * Implement basic Pulp Galaxy API v3 client.
    * Implement Collection and CollectionVersion viewsets.

--- galaxy_api/api/urls.py
+++ galaxy_api/api/urls.py
@@ -3,9 +3,11 @@
 from django.urls import include, path
 
 from .ui import urls as ui_urls
+from .v3 import urls as v3_urls
 
 
 app_name = 'api'
 urlpatterns = [
+    path('v3/', include(v3_urls, namespace='v3')),
     path('v3/_ui/', include(ui_urls, namespace='ui')),
 ]
