8f34655a955fdd4ab84136c2f565fb85ceccb552
David Newswanger <dnewswan@redhat.com>
2019-08-28 15:13:47 -0400

    add admin form for adding namespaces and tenants

--- galaxy_api/auth/admin.py
+++ galaxy_api/auth/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from galaxy_api.auth import models
+from galaxy_api.api import models as api_models
 
 
 @admin.register(models.User)
 class UserAdmin(BaseUserAdmin):
+    pass
+
+
+@admin.register(models.Tenant)
+class TenantAdmin(admin.ModelAdmin):
     pass
