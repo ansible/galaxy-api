ac0be19ac4cb0e04ba2dc63afd67ef3c6e7de784
Alexander Saprykin <osapryki@redhat.com>
2019-08-29 18:24:53 +0200

    API(UI): Add tags list endpoint
    Closes: ansible/galaxy-dev#64

--- galaxy_api/api/ui/viewsets/__init__.py
+++ galaxy_api/api/ui/viewsets/__init__.py
@@ -1,10 +1,11 @@
 from .namespace import NamespaceViewSet
 from .collection import CollectionViewSet, CollectionVersionViewSet, CollectionImportViewSet
-
+from .tags import TagsViewSet
 
 __all__ = (
     'NamespaceViewSet',
     'CollectionViewSet',
     'CollectionVersionViewSet',
     'CollectionImportViewSet',
+    'TagsViewSet',
 )
---
+++ galaxy_api/api/ui/viewsets/tags.py
@@ -0,0 +1,21 @@
+from rest_framework import viewsets
+import galaxy_pulp
+
+from galaxy_api.common import pulp
+
+
+class TagsViewSet(viewsets.GenericViewSet):
+
+    def list(self, request, *args, **kwargs):
+        self.paginator.init_from_request(request)
+
+        params = self.request.query_params.dict()
+        params.update({
+            'offset': self.paginator.offset,
+            'limit': self.paginator.limit,
+        })
+
+        api = galaxy_pulp.PulpTagsApi(pulp.get_client())
+        response = api.list(**params)
+
+        return self.paginator.paginate_proxy_response(response.results, response.count)
