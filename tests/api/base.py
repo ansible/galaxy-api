18e3b1b443851bd0f35b4aeb5534dcce53ca48c7
Alexander Saprykin <osapryki@redhat.com>
2019-09-10 17:01:54 +0200

    Refactor and fix test suite database usage (#77)
    * BaseTestCase inherits rest framework base test case class.
    * Remove obsolete `conftest.py` module.
    * Add helper functions to create database resources.

--- tests/api/base.py
+++ tests/api/base.py
@@ -1,26 +1,46 @@
-import unittest
 from unittest import mock
 
 from django.conf import settings
-from rest_framework.test import APIClient
+from rest_framework.test import APIClient, APITestCase
 
-from galaxy_api.auth.auth import RHEntitlementRequired
-from galaxy_api.auth.models import User
+from galaxy_api.api import models as models
+from galaxy_api.auth import auth
+from galaxy_api.auth import models as auth_models
 
 
-class BaseTestCase(unittest.TestCase):
+API_PREFIX = settings.API_PATH_PREFIX.strip("/")
+
+
+class BaseTestCase(APITestCase):
     def setUp(self):
         self.client = APIClient()
 
-        user = User(username="testuser")
-        self.client.force_authenticate(user=user)
+        self.user = self._create_user('test')
+        self.client.force_authenticate(user=self.user)
 
         # Permission mock
         patcher = mock.patch.object(
-            RHEntitlementRequired, "has_permission", return_value=True
+            auth.RHEntitlementRequired, "has_permission", return_value=True
         )
         patcher.start()
         self.addCleanup(patcher.stop)
 
+    @staticmethod
+    def _create_user(username):
+        return auth_models.User.objects.create(username=username)
 
-API_PREFIX = settings.API_PATH_PREFIX.strip("/")
+    @staticmethod
+    def _create_group(scope, name, users=None):
+        group = auth_models.Group.objects.create_identity(scope, name)
+        if isinstance(users, auth_models.User):
+            users = [users]
+        group.user_set.add(*users)
+        return group
+
+    @staticmethod
+    def _create_namespace(name, groups=None):
+        namespace = models.Namespace.objects.create(name=name)
+        if isinstance(groups, auth_models.Group):
+            groups = [groups]
+        namespace.groups.add(*groups)
+        return namespace
--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from rest_framework import test as drf_test
-
-
-@pytest.fixture()
-def api_client():
-    return drf_test.APIClient()
