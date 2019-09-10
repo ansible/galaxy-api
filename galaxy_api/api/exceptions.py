c45422d2487297d5dc9e6573673ba6baac6c6ede
Alexander Saprykin <osapryki@redhat.com>
2019-09-10 20:14:45 +0200

    Handle exceptions raised by galaxy_pulp bindings

--- galaxy_api/api/exceptions.py
+++ galaxy_api/api/exceptions.py
@@ -1,5 +1,6 @@
+import galaxy_pulp
 from django.core.exceptions import PermissionDenied
-from django.http import Http404
+from django.http import Http404, HttpResponse
 
 from rest_framework import exceptions
 from rest_framework.response import Response
@@ -28,17 +29,7 @@ def _get_errors(detail, *, status, title, source=None):
         yield error
 
 
-def exception_handler(exc, context):
-    """Custom exception handler."""
-
-    if isinstance(exc, Http404):
-        exc = exceptions.NotFound()
-    elif isinstance(exc, PermissionDenied):
-        exc = exceptions.PermissionDenied()
-
-    if not isinstance(exc, exceptions.APIException):
-        return None
-
+def _handle_drf_api_exception(exc):
     headers = {}
     if getattr(exc, 'auth_header', None):
         headers['WWW-Authenticate'] = exc.auth_header
@@ -49,3 +40,23 @@ def exception_handler(exc, context):
     errors = _get_errors(exc.detail, status=exc.status_code, title=title)
     data = {'errors': list(errors)}
     return Response(data, status=exc.status_code, headers=headers)
+
+
+def _handle_api_client_exception(exc):
+    return HttpResponse(exc.body, status=exc.status, content_type=exc.headers['Content-Type'])
+
+
+def exception_handler(exc, context):
+    """Custom exception handler."""
+    if isinstance(exc, galaxy_pulp.ApiException):
+        return _handle_api_client_exception(exc)
+
+    if isinstance(exc, Http404):
+        exc = exceptions.NotFound()
+    elif isinstance(exc, PermissionDenied):
+        exc = exceptions.PermissionDenied()
+
+    if isinstance(exc, exceptions.APIException):
+        return _handle_drf_api_exception(exc)
+
+    return None
