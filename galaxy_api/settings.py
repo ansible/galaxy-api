1c05752fd27ac8c24198130e2f198a79d5297004
Alexander Saprykin <osapryki@redhat.com>
2019-09-12 21:09:01 +0200

    Implement artifact download endpoint

--- galaxy_api/settings.py
+++ galaxy_api/settings.py
@@ -130,6 +130,10 @@ PULP_API_PORT = 8000
 PULP_API_USER = 'admin'
 PULP_API_PASSWORD = 'admin'
 
+PULP_CONTENT_HOST = 'pulp-content-app'
+PULP_CONTENT_PORT = 24816
+PULP_CONTENT_PATH_PREFIX = '/api/automation-hub/artifacts/collections/'
+
 ################################################################################
 #                                   FOOTER                                     #
 ################################################################################
