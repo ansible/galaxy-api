0dcf4ef4cc62de017582df0ef3e9eda1e2648979
Alexander Saprykin <osapryki@redhat.com>
2019-11-13 19:39:51 +0100

    Fix static serving for production

--- galaxy_api/urls.py
+++ galaxy_api/urls.py
@@ -1,9 +1,9 @@
 """URLs Configuration."""
 
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.static import serve as serve_static
 
 
 api_prefix = settings.API_PATH_PREFIX.strip('/')
@@ -13,7 +13,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-if settings.STATIC_ROOT:
-    urlpatterns += static(
-        settings.STATIC_URL, document_root=settings.STATIC_ROOT
-    )
+# Serve static files for admin site
+if not settings.DEBUG and settings.STATIC_ROOT:
+    urlpatterns += [
+        path(
+            '{prefix}<path:path>'.format(prefix=settings.STATIC_URL.lstrip('/')),
+            serve_static,
+            kwargs={'document_root': settings.STATIC_ROOT},
+        )
+    ]
