0b1cab203de7b815972992585903220d07763050
David Newswanger <dnewswan@redhat.com>
2019-11-13 11:57:31 -0500

    Show users in group admin form, make groups searchable on namespace form (#130)

--- galaxy_api/api/admin.py
+++ galaxy_api/api/admin.py
@@ -5,6 +5,7 @@ from galaxy_api.api import models as api_models
 @admin.register(api_models.Namespace)
 class NamespaceAdmin(admin.ModelAdmin):
     fields = ('name', 'company', 'email', 'avatar_url', 'description', 'groups')
+    autocomplete_fields = ['groups']
 
 
 @admin.register(api_models.NamespaceLink)
--- galaxy_api/auth/admin.py
+++ galaxy_api/auth/admin.py
@@ -8,6 +8,31 @@ class UserAdmin(django_auth_admin.UserAdmin):
     pass
 
 
+class UserInLine(admin.TabularInline):
+    model = models.Group.user_set.through
+    fields = ('user', 'username', 'email_address', 'first_name', 'last_name',)
+    readonly_fields = ('username', 'email_address', 'first_name', 'last_name',)
+
+    verbose_name = "User"
+    verbose_name_plural = "Group Members"
+
+    autocomplete_fields = ['user']
+
+    def username(self, instance):
+        return instance.user.username
+
+    def email_address(self, instance):
+        return instance.user.email_address
+
+    def first_name(self, instance):
+        return instance.user.first_name
+
+    def last_name(self, instance):
+        return instance.user.last_name
+
+    extra = 0
+
+
 @admin.register(models.Group)
 class GroupAdmin(django_auth_admin.GroupAdmin):
-    pass
+    inlines = [UserInLine]
