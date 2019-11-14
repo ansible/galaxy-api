ea164dbedbf72d76e05318cfe1a670831632c285
Roman Blanco <rblanco@redhat.com>
2019-11-14 11:24:01 +0000

    Adding unit tests for _ui certification collection API (#121)

--- tests/api/test_api_ui_collection_viewsets.py
+++ tests/api/test_api_ui_collection_viewsets.py
@@ -1,6 +1,8 @@
 from unittest import mock
 
-from .base import BaseTestCase
+from galaxy_pulp.models import CertificationInfo
+
+from .base import BaseTestCase, API_PREFIX
 
 
 class TestCollectionVersionViewSet(BaseTestCase):
@@ -14,3 +16,19 @@ class TestCollectionVersionViewSet(BaseTestCase):
         patcher = mock.patch("galaxy_pulp.GalaxyCollectionVersionsApi", spec=True)
         self.versions_api = patcher.start().return_value
         self.addCleanup(patcher.stop)
+
+    def test_set_certified(self):
+        self.versions_api.set_certified.return_value = {}
+
+        response = self.client.put(
+            f"/{API_PREFIX}/v3/_ui/collection-versions/ansible/nginx/1.2.3/certified/",
+            data={'certification': 'certified'},
+            format='json',
+        )
+
+        assert response.status_code == 200
+        assert response.data == {}
+        self.versions_api.set_certified.assert_called_once_with(
+            prefix=API_PREFIX, namespace="ansible", name="nginx",
+            version="1.2.3", certification_info=CertificationInfo('certified')
+        )
