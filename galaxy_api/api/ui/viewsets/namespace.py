5538da38e9e487a5ceb6b073dab2d33b754714b8
Roman Blanco <rblanco@redhat.com>
2020-01-30 13:34:08 +0000

    Accept not yet registered ids while creating namespace (#145)
    * create not yet registered groups
    
    * Review responses

--- galaxy_api/api/ui/viewsets/namespace.py
+++ galaxy_api/api/ui/viewsets/namespace.py
@@ -3,10 +3,19 @@ from django_filters.rest_framework import filterset, DjangoFilterBackend
 
 from rest_framework import mixins
 from rest_framework import viewsets
+from rest_framework import status
+
 from rest_framework.settings import api_settings
+from rest_framework.response import Response
 
 from galaxy_api.api import models, permissions
 from galaxy_api.api.ui import serializers
+
+from galaxy_api.auth import auth as auth
+from galaxy_api.auth import models as auth_models
+
+
+RH_PE_ACCOUNT_SCOPE = 'system:partner-engineers'
 
 
 class NamespaceFilter(filterset.FilterSet):
@@ -37,7 +46,6 @@ class NamespaceFilter(filterset.FilterSet):
 class NamespaceViewSet(
     mixins.ListModelMixin,
     mixins.RetrieveModelMixin,
-    mixins.CreateModelMixin,
     mixins.UpdateModelMixin,
     viewsets.GenericViewSet,
 ):
@@ -49,6 +57,23 @@ class NamespaceViewSet(
     filter_backends = (DjangoFilterBackend,)
 
     filterset_class = NamespaceFilter
+
+    def create(self, request, *args, **kwargs):
+        groups = []
+        for account in request.data['groups']:
+            if account == RH_PE_ACCOUNT_SCOPE:
+                groups.append(account)
+            else:
+                group, _ = auth_models.Group.objects.get_or_create_identity(
+                        auth.RH_ACCOUNT_SCOPE, account)
+                groups.append(group.name)
+        request.data['groups'] = groups
+
+        serializer = serializers.NamespaceSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get_serializer_class(self):
         if self.action == 'list':
